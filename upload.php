62e528fb9c752370849a70928e1553788890322f
Klaudia164 <66058455+Klaudia164@users.noreply.github.com>
2021-12-02 12:23:44 +0100

    file upload

--- upload.php
+++ upload.php
@@ -1,26 +1,62 @@
 <?php
 
-if(isset($_FILES["fileToUpload"])){
-  $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-    }
-    echo '<pre>';
-    print_r($_FILES);
-    echo '</pre>';
-}
+    $i = 0;
+    $errors = array();
 
+if(isset($_FILES["fileToUpload"])) {
+
+  echo '<pre>';
+  print_r($_FILES["fileToUpload"]);
+  echo '</pre>';
+
+  $target_dir = "uploads/";
+
+  $allowed_filetypes = array('image/png', 'image/jpg', 'image/jpeg');
+
+
+    foreach($_FILES["fileToUpload"]["name"] as $key => $name){
+      $target_file = $target_dir . basename($name);  
+
+      if ($_FILES["fileToUpload"]["size"][$key] > 102400) {
+        $errors[$key][] = "A $name túl nagy méretű, 100KB-nál nem lehet nagyobb";
+      }
+      elseif ($_FILES["fileToUpload"]["size"][$key] < 1024) {
+        $errors[$key][] = "A $name túl kicsi méretű, 1KB-nál nem lehet kisebb";
+      }
+      if (!in_array($_FILES["fileToUpload"]["type"][$key], $allowed_filetypes)) {
+        $errors[$key][] = "A $name file nem jpg vagy png.";
+      }
+    
+    if(!isset($errors[$key])){
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$key], $target_file)) {
+       $i++;
+      }
+    }  
+  }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <body>
 
+  <?php
+
+  if ($i > 0) echo "$i fájl feltöltve";
+  
+  if($errors) {
+    foreach ($errors as $error) {
+      foreach ($error as $errorMsg) {
+        echo "$errorMsg <br>";
+      }
+    }
+  }
+
+  ?>
+
 <form action="upload.php" method="post" enctype="multipart/form-data">
   Select image to upload: 
-  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="file" name="fileToUpload[]" id="fileToUpload" multiple>
   <input type="submit" value="Upload Image" name="submit">
 </form>
 
